ef67458aadc85adafd6f341390ae468330e2e486
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-09-23 12:38:47 +0300

    Initial project

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::println;
+
+fn main() {
+    let product_name = "Earphones";
+    let price = 9000.0;
+    let in_stock = true;
+    let category = "Electronics";
+
+    println!("{} | {} rub. | {} | in stock: {}", product_name, price, category, in_stock)
+}
