46f414d5fabfda928407b0ae21c23841e530f745
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-09-23 12:51:34 +0300

    Step 3: Mutable and Shadowing

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,49 @@ fn main() {
     let in_stock = true;
     let category = "Electronics";
 
-    println!("{} | {} rub. | {} | in stock: {}", product_name, price, category, in_stock)
+    println!("{} | {} rub. | {} | in stock: {}", product_name, price, category, in_stock);
+
+    let stock_before = 10;
+
+    let stock_after = {
+        let mut stock = stock_before;
+        stock -= 3;
+        stock -= 1;
+        stock
+    };
+
+    println!("Before: {} | After: {}", stock_before, stock_after);
+
+    let init_price = 1500;
+
+    let final_price = {
+        let mut price2 = init_price;
+        price2 -= 500;
+        price2 -= 300;
+        price2
+    };
+
+    println!("Initial price: {} | Final price: {}", init_price, final_price);
+
+    let price1 = 1000;
+
+    {
+        let price1 = price1 + 500;
+        println!("Inside block: {}", price1);
+
+    }
+
+    println!("Outside block: {}", price1);
+
+    let price3 = "1590.0";
+
+    let final_price2 = {
+        let mut price3: f32 = price3.parse().unwrap();
+        price3 *= 1.15;
+        price3
+    };
+
+    println!("FINAL PRICE: {}", final_price2);
+
+
 }
